fa10c4414e6fe90629a8205d247c5417ae750857
Patrick-Mickler <107968445+Patrick-Mickler@users.noreply.github.com>
2022-06-22 21:25:47 -0400

    created index and script files

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerPlay(){
+    const number = Math.ceil(Math.random()*3);
+
+    if (number == 1){ 
+    console.log(rock);
+    } else if (number == 2){
+    console.log(paper);
+    } else if (number == 3){
+    console.log(scissors);
+    } 
+    
+}
+console.log(number);
